09067b8387415bd5056af7fe52f96b6cc0209211
LMH <leemh9256@gmail.com>
2024-02-26 16:47:02 +0900

    리액트 / 리액트 라우터로 spa 개발 / 라우팅, 라우터 적용 및 기본 사용법, URL 파라미터와 쿼리스트링, 중첩된 라우트, 라우터 부가 기능

--- react/router-react/src/App.js
+++ react/router-react/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profiles from './pages/Profiles';
+import Articles from './pages/Articles';
+import Article from './pages/Article';
+import Layout from './pages/Layout';
+import NotFound from './pages/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profiles/:id' element={<Profiles/>}/>
+      </Route>
+
+      <Route path='/articles' element={<Articles/>}>
+        <Route path='/articles/:id' element={<Article/>}/>
+      </Route>
+
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ react/router-react/src/pages/Profiles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const data = {
+    user1 : {
+        name: '양영보',
+        desc: '시니어 개발자'
+    },
+    user2 : {
+        name: '이무현',
+        desc: '주니어 개발자'
+    }
+}
+
+const Profiles = () => {
+    const params = useParams();
+    const profile = data[params.id];
+    return (
+        <div>
+            <h1>사용자 프로필</h1>
+            <p>profile id : {params.id}</p>
+            {profile ? <div><h2>{profile.name}</h2><p>{profile.desc}</p></div> : <div>존재하지 않는 프로필입니다.</div>}
+            {/* <div>
+                <h2>{profile.name}</h2>
+                <p>{profile.desc}</p>
+            </div>
+            <div>
+                존재하지 않는 프로필입니다.
+            </div> */}
+        </div>
+    );
+};
+
+export default Profiles;
