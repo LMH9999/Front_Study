b8b88f0df57d96e9852df94827be2e3fd6452cc4
LMH <leemh9256@gmail.com>
2024-02-26 10:27:00 +0900

    리액트 / 컴포넌트 성능 최적화 / memo, virtualized 사용

--- react/todo-app2/src/components/TodoList.js
+++ react/todo-app2/src/components/TodoList.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css'
+import {List} from 'react-virtualized';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+    const rowRender = useCallback(({index,key,style})=> {
+        // console.log(index,key,style);
+        const todo = todos[index];
+        return (
+            <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style}/>
+        );
+    },[todos,onRemove,onToggle]);
     return (
-        <div className='TodoList'>
-            {
-                todos.map(todo=><TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>)
-            }
-        </div>
+        // <div className='TodoList'>
+        //     {
+        //         todos.map(todo=><TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>)
+        //     }
+        // </div>
+
+        <List 
+            className='TodoList' width={512} height={513} rowCount={todos.length} 
+            rowHeight={57} rowRenderer={rowRender} list={todos} style={{outline:'none'}}
+        />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
