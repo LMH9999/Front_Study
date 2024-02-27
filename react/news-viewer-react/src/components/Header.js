3d1a9f41aed97288816b7a5c72f2502cb384dc47
LMH <leemh9256@gmail.com>
2024-02-27 16:51:19 +0900

    오류 수정

--- react/news-viewer-react/src/components/Header.js
+++ react/news-viewer-react/src/components/Header.js
@@ -3,9 +3,9 @@ import { NavLink } from '../../node_modules/react-router-dom/dist/index';
 
 const Header = () => {
     return (
-        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-            <h5 class="my-0 mr-md-auto font-weight-bold">News API</h5>
-            <nav class="my-2 my-md-0 mr-md-3">
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+            <h5 className="my-0 mr-md-auto font-weight-bold">News API</h5>
+            <nav className="my-2 my-md-0 mr-md-3">
                 <NavLink className={({isActive})=>(isActive ? "p-2 text-dark active" : "p-2 text-dark")} to="/">Home</NavLink>
                 <NavLink className={({isActive})=>(isActive ? "p-2 text-dark active" : "p-2 text-dark")} to="/news">뉴스</NavLink>
             </nav>
