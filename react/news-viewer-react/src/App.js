c2a1f078a1c270d467d3216cb7d53001aa753ff2
LMH <leemh9256@gmail.com>
2024-02-27 13:28:57 +0900

    리액트 / 외부 api 연동 뉴스 뷰어 / 기본 설정 및 api 테스트

--- react/news-viewer-react/src/App.js
+++ react/news-viewer-react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from '../node_modules/axios/index';
 
-function App() {
+const App = () => {
+  const [data,setData] = useState(null);
+  const onClick = async () =>{
+    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=2a3f86554a2140cab6fae81c466891fd');
+    setData(response)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClick}>불러오기</button>
+      <div>
+        {data && <textarea value={JSON.stringify(data)}/>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
